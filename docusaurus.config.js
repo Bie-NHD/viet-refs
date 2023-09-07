db0d00f3d5cd7480fb8af59b28a4afea45dfd722
Duyên Ng <huongduyen.work@gmail.com>
2023-09-08 00:27:52 +0700

    add search bar
    - add plugins
    - config i18n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,20 +74,24 @@ const config = {
           alt: "My Site Logo",
           src: "img/logo.svg",
         },
-        // items: [
-        //   {
-        //     type: "docSidebar",
-        //     sidebarId: "tutorialSidebar",
-        //     position: "left",
-        //     label: "Tutorial",
-        //   },
-        //   { to: "/blog", label: "Blog", position: "left" },
-        //   {
-        //     href: "https://github.com/facebook/docusaurus",
-        //     label: "GitHub",
-        //     position: "right",
-        //   },
-        // ],
+        items: [
+          //   {
+          //     type: "docSidebar",
+          //     sidebarId: "tutorialSidebar",
+          //     position: "left",
+          //     label: "Tutorial",
+          //   },
+          //   { to: "/blog", label: "Blog", position: "left" },
+          //   {
+          //     href: "https://github.com/facebook/docusaurus",
+          //     label: "GitHub",
+          //     position: "right",
+          //   },
+          {
+            type: "search",
+            position: "right",
+          },
+        ],
       },
       announcementBar: {
         id: "announce",
@@ -146,6 +150,46 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        // whether to index docs pages
+        indexDocs: true,
+
+        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
+        // 0 disables this feature.
+        // 1 indexes the direct parent category in the sidebar of a doc page
+        // 2 indexes up to two nested parent categories of a doc page
+        // 3...
+        //
+        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
+        indexDocSidebarParentCategories: 0,
+
+        // whether to index blog pages
+        indexBlog: false,
+
+        // whether to index static pages
+        // /404.html is never indexed
+        indexPages: false,
+
+        // language of your documentation, see next section
+        language: "vi",
+
+        // setting this to "none" will prevent the default CSS to be included. The default CSS
+        // comes from autocomplete-theme-classic, which you can read more about here:
+        // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
+        // When you want to overwrite CSS variables defined by the default theme, make sure to suffix your
+        // overwrites with `!important`, because they might otherwise not be applied as expected. See the
+        // following comment for more information: https://github.com/cmfcmf/docusaurus-search-local/issues/107#issuecomment-1119831938.
+        style: undefined,
+
+        // The maximum number of search results shown to the user. This does _not_ affect performance of
+        // searches, but simply does not display additional search results that have been found.
+        maxSearchResults: 8,
+      },
+    ],
+  ],
 };
 
 module.exports = config;
